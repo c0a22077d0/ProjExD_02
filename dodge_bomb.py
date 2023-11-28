3a8a6a9eab94c0a5cf23278e0e9bc49daa8aba3d
c0a22077 <c0a22077d0@edu.teu.ac.jp>
2023-11-28 14:34:42 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,14 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 900, 600
+
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 
 
 def main():
@@ -12,21 +19,35 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+
     bb_img = pg.Surface((20, 20))  #練習1　透明色
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #練習　半径10の赤い円
+
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH) #練習2
     bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for k, tpl in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
